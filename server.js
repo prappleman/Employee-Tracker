3286c0db17205f662df4ea3f091c072adf5026f6
parkerrappleye <parker.rappleye1@gmail.com>
2024-03-19 17:03:41 -0600

    adde sequelize sync

--- server.js
+++ server.js
@@ -20,13 +20,3 @@ sequelize.sync({ force: true }).then(() => {
   });
 });
 
-// Handle 404 errors
-app.use((req, res, next) => {
-  res.status(404).send('404 - Not Found');
-});
-
-// Error handler middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('500 - Internal Server Error');
-});
