55a723c54e17df820b98b6ef7fc33351adf68915
parkerrappleye <parker.rappleye1@gmail.com>
2024-03-21 20:26:14 -0600

    updated seeds to parks and rec characters

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,37 +1,35 @@
--- Department table and corresponding values
+-- Department table and corresponding values for Parks and Recreation
 INSERT INTO department (name)
-VALUES ("Management"),
-        ("Sales"),
-        ("Accounting"),
+VALUES ("Parks and Recreation"),
+        ("City Planning"),
+        ("Finance"),
         ("Human Resources"),
-        ("Customer Service"),
-        ("Quality Assurance"),
-        ("Office Administration");
+        ("Parks Maintenance"),
+        ("Public Relations"),
+        ("City Management");
 
--- Role table and corresponding values
+-- Role table and corresponding values for Parks and Recreation
 INSERT INTO role (title, salary, department_id)
-VALUES ("Branch Manger", 70000, 1),
-        ("Assistant to Branch Manager", 60000, 1),
-        ("Salesman", 50000, 2),
+VALUES ("Parks Director", 80000, 1),
+        ("Deputy Parks Director", 70000, 1),
+        ("City Planner", 60000, 2),
         ("Accountant", 65000, 3),
-        ("Human Resources Representative", 55000, 4),
-        ("Customer Service Specialist", 55000, 5),
-        ("QA Director", 50000, 6),
-        ("Receptionist", 45000, 7);
+        ("HR Specialist", 55000, 4),
+        ("Parks Maintenance Supervisor", 55000, 5),
+        ("Public Relations Manager", 60000, 6),
+        ("City Manager", 90000, 7);
 
--- Employee table and corresponding values
+-- Employee table and corresponding values for Parks and Recreation
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Michael", "Scott", 1, 1),
-        ("Jim", "Halpert", 3, NULL),
-        ("Dwight", "Schrute", 2, 2),
-        ("Stanley", "Hudson", 3, NULL),
-        ("Phyllis", "Vance", 3, NULL),
-        ("Oscar", "Martinez", 4, NULL),
-        ("Angela", "Martin", 4, NULL),
-        ("Kevin", "Malone", 4, NULL),
-        ("Toby", "Flenderson", 5, NULL),
-        ("Kelly", "Kapoor", 6, NULL),
-        ("Creed", "Bratton", 7, NULL),
-        ("Pam", "Beesly", 8, NULL),
-        ("Erin", "Hannon", 8, NULL);
+VALUES ("Leslie", "Knope", 1, NULL),
+        ("Ron", "Swanson", 2, NULL),
+        ("Tom", "Haverford", 3, 1),
+        ("Ann", "Perkins", 4, NULL),
+        ("Ben", "Wyatt", 7, 6),
+        ("April", "Ludgate", 5, 2),
+        ("Andy", "Dwyer", 6, 2),
+        ("Donna", "Meagle", 5, NULL),
+        ("Jerry", "Gergich", 6, NULL),
+        ("Chris", "Traeger", 7, 6),
+        ("Craig", "Middlebrooks", 7, 6);
 
